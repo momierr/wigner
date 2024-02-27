4bbf6f96694495192ce0ade47083d525b622f56e
rodolphe <momier.rodolphe@gmail.com>
2024-02-27 09:24:56 +0100

    commit 270224

--- wigner3j.py
+++ wigner3j.py
@@ -4,25 +4,14 @@ import warnings
 from scipy.special import gammaln
 
 
-def delta(j123):
-    j1 = j123[0]
-    j2 = j123[1]
-    j3 = j123[2]
-    return np.sqrt(math.factorial(j1 + j2 - j3) * math.factorial(j1 - j2 + j3) * math.factorial(-j1 + j2 + j3) / math.factorial(j1 + j2 + j3 + 1))
+def delta(a,b,c):
+    return np.sqrt(math.factorial(a + b - c) * math.factorial(a - b + c) * math.factorial(-a + b + c) / math.factorial(a + b + c + 1))
     
-def wigner3j(j123, m123):
+def wigner3j(j1, j2, j3, m1, m2, m3):
     
-    j1 = j123[0]
-    j2 = j123[1]
-    j3 = j123[2]
-    
-    m1 = m123[0]
-    m2 = m123[1]
-    m3 = m123[2]
-    
-    
-    j123 = np.array(j123)
-    m123 = np.array(m123)
+    j123 = np.array([j1,j2,j3])
+    m123 = np.array([m1,m2,m3])
+
     
     if (j123<0).sum() != 0:
         warnings.warn("All j values must be > 0")
@@ -53,14 +42,8 @@ def wigner3j(j123, m123):
     
     
     w3j = (-1)**(j1 - j2 - m3)
-    w3j *= delta(j123)
+    w3j *= delta(j1,j2,j3)
     
-    print(j1-m1)
-    print(j1+m1)
-    print(j2-m2)
-    print(j2+m2)
-    print(j3-m3)
-    print(j3+m3)
     
     w3j *= np.sqrt(np.exp(gammaln(j1-m1 +1)) * np.exp(gammaln(j1+m1+1)) * np.exp(gammaln(j2-m2+1)) * np.exp(gammaln(j2+m2+1)) *np.exp(gammaln(j3-m3+1)) *np.exp(gammaln(j3+m3+1)))
         
@@ -77,8 +60,37 @@ def wigner3j(j123, m123):
     return w3j
 
 
+def cg(j1,m1,j2,m2,j3,m3):
+    phase = (-1)**(-j1+j2-m3)
+    return phase*np.sqrt(2*j3+1)*wigner3j(j1,j2,j3,m1,m2,-m3)
 
 
-
+def wigner6j(j1,j2,j3,j4,j5,j6):
+    
+    w6j = 0
+    t1 = j1+j2+j3
+    t2 = j1+j5+j6
+    t3 = j4+j2+j6
+    t4 = j4+j5+j3
 
     
+    inf = max([t1, t2, t3, t4])
+
+    t5 = j1 + j2 + j4 + j5
+    t6 = j2 + j3 + j5 + j6
+    t7 = j3 + j1 + j6 + j4
+    
+    sup = min([t5, t6, t7])
+
+ 
+    w6j = delta(j1,j2,j3) * delta(j1,j5,j6) * delta(j4,j2,j6) * delta(j4,j5,j3)
+    sum = 0
+    
+    for k in range(inf, sup + 1):
+        X = np.exp(gammaln(k-t1+1)) * np.exp(gammaln(k-t2+1)) * np.exp(gammaln(k-t3+1)) * np.exp(gammaln(k-t4+1)) * np.exp(gammaln(t5-k+1)) * np.exp(gammaln(t6-k+1)) * np.exp(gammaln(t7-k+1))  
+        sum += (-1)**k * np.exp(gammaln(k+2)) / X
+        
+    w6j *= sum
+    
+    return w6j
+
